b88b4ca65f2e00a3f04649dfbd831ca8b27e1441
Aleksey Popov <alekseypopov.dev@gmail.com>
2025-03-16 23:15:54 +0700

    feat: update Home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,8 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { YMaps, Map, Placemark } from "@pbe/react-yandex-maps";
 
 import { features, plates } from "./response";
 
@@ -8,9 +11,9 @@ const Home = () => {
     <>
       <section>
         <div className="container">
-          <div className="grid grid-flow-col grid-rows-2 gap-4">
+          <div className="grid grid-flow-col-dense grid-cols-2 grid-rows-[auto,1fr] gap-4">
             <Image
-              className="row-span-2 h-full rounded bg-[rgb(226,226,226)] object-contain shadow"
+              className="col-span-2 row-span-1 h-full rounded bg-[rgb(226,226,226)] object-cover object-left shadow md:row-span-2"
               src="/hero-1.webp"
               width={916}
               height={429}
@@ -32,7 +35,8 @@ const Home = () => {
               alt="small-2"
             />
           </div>
-          <div className="grid grid-cols-3 gap-4 py-6">
+
+          <div className="grid grid-cols-1 grid-rows-3 gap-4 pb-8 pt-4 sm:grid-cols-3 sm:grid-rows-1">
             {features.map(
               (feature: {
                 title: string;
@@ -41,7 +45,7 @@ const Home = () => {
               }) => (
                 <div
                   key={feature.title}
-                  className="flex items-center gap-3 rounded bg-white p-5 shadow"
+                  className="col-span-1 flex items-center gap-3 rounded bg-white p-5 shadow"
                 >
                   <svg
                     className="size-10 fill-neutral-950"
@@ -53,11 +57,13 @@ const Home = () => {
                   >
                     <path d={feature.icon} />
                   </svg>
-                  <div>
-                    <h3 className="text-lg font-bold leading-none text-neutral-950">
+                  <div className="flex flex-col justify-center">
+                    <h3 className="text-lg font-extrabold leading-none text-neutral-950">
                       {feature.title}
                     </h3>
-                    <p className="text-neutral-500">{feature.description}</p>
+                    <p className="block text-neutral-500 sm:hidden xl:block">
+                      {feature.description}
+                    </p>
                   </div>
                 </div>
               ),
@@ -67,11 +73,11 @@ const Home = () => {
       </section>
 
       <section>
-        <div className="container py-4">
-          <h2 className="inline-block rounded bg-primary px-4 py-2 text-2xl font-black text-neutral-50">
+        <div className="container py-8">
+          <h2 className="inline-block rounded bg-primary px-4 py-2 text-xl font-black text-neutral-50 sm:text-2xl">
             Популярные категории
           </h2>
-          <div className="grid grid-flow-row-dense grid-cols-3 grid-rows-3 gap-4 py-6">
+          <div className="grid grid-flow-row-dense auto-rows-auto grid-cols-2 gap-4 pt-6 lg:grid-cols-3">
             {plates.map((plate) => (
               <div
                 key={plate.title}
@@ -79,7 +85,7 @@ const Home = () => {
               >
                 <Link
                   href={`/${plate.title}`}
-                  className="text-lg font-extrabold uppercase leading-none text-secondary"
+                  className="text-lg font-extrabold leading-none text-secondary"
                 >
                   {plate.title}
                 </Link>
@@ -99,11 +105,40 @@ const Home = () => {
         </div>
       </section>
 
-      <section>
+      {/* <section>
         <div className="container py-4">
           <h2 className="inline-block rounded bg-primary px-4 py-2 text-2xl font-black text-neutral-50">
             Новости и акции
           </h2>
+        </div>
+      </section> */}
+
+      <section>
+        <div className="container py-8">
+          <h2 className="inline-block rounded bg-primary px-4 py-2 text-xl font-black text-neutral-50 sm:text-2xl">
+            Мы тут
+          </h2>
+          <div className="mt-6 h-[400px] w-full overflow-hidden rounded shadow-md">
+            <YMaps>
+              <Map
+                defaultState={{
+                  center: [55.006786, 82.972837],
+                  zoom: 13,
+                  controls: ["zoomControl", "fullscreenControl"],
+                }}
+                modules={["control.ZoomControl", "control.FullscreenControl"]}
+                className="h-full w-full"
+              >
+                <Placemark
+                  geometry={[55.006786, 82.972837]}
+                  options={{
+                    preset: "islands#redDotIcon",
+                    iconColor: "#e70a40",
+                  }}
+                />
+              </Map>
+            </YMaps>
+          </div>
         </div>
       </section>
     </>
